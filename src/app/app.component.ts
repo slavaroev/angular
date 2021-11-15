633df2be8791b93d3bfa9c7531ca9c6c4deb22d7
viacheslav <roiev.v@soft2bet.com>
2021-11-15 21:08:34 +0200

    Implemented post form and post card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+  id: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  public posts: Post[] = [
+    { title: 'Title for post 1', text: 'Text for post 1', id: 1 },
+    { title: 'Title for post 2', text: 'Text for post 2', id: 2 },
+  ];
+
+  addPost(post: Post) {
+    this.posts.unshift(post);
+  }
 }
